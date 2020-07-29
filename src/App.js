c8a755da86360518f1198b6887da7798cf75f538
Shahzad Umar Baig <ShahzadUBaig@gmail.com>
2020-07-29 14:59:34 +0500

    Added a new component for learning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-
+import { Greet } from "./Components/Greet";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="App">
+        <Greet />
+      </div>
+    );
   }
 }
 
