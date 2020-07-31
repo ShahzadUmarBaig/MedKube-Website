c250083d04b74d2704186d73c6c259874026bcb3
Shahzad Umar Baig <ShahzadUBaig@gmail.com>
2020-07-31 16:41:19 +0500

    I have added another functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Greet } from "./Components/Greet";
+import Welcome from "./Components/Welcome";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <Greet />
+        <Welcome />
+        <h2>It is learning process</h2>
       </div>
     );
   }
